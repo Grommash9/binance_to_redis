77aea571ca7124e3c100358a017583f96a608fb1
prudn <prundikov21@icloud.com>
2022-07-10 16:26:09 +0300

    task

--- get_data_from_redis.py
+++ get_data_from_redis.py
@@ -1,27 +1,13 @@
-import redis
+# импортируем модуль requests что бы слать запросы, если пишет что его
+# нет можно установить его командой pip install requests введенной в Terminal PyCharm
+import requests
 
-redis = redis.Redis(decode_responses=True)
+# получаем данные о торовли с бинанс и делаем из них список
+data = requests.get('https://api1.binance.com/api/v3/exchangeInfo').json()
 
-# так мы можем получить все ключи которые начинаються с бинанс и есть у нас
-print(redis.keys('binance_*'))
-
-# в цикле можем пройтись по всем названиям ключей и получить их значения
-for keys in redis.keys('binance_*'):
-    print(keys, redis.get(keys))
-
-# что бы получить данные по определенной валюте пишем
-print(redis.get('binance_VIDTBUSD'))
-
-# в цикле можем пройтись по всем названиям ключей и получить их значения удалив подпись бинанс
-for keys in redis.keys('binance_*'):
-    print(keys[8:], redis.get(keys))
-
-
-# функция для получения словаря с названиями криптовалютных пар и значениями их последней цены
-def get_binance_dict() -> dict:
-    binance_dict = dict()
-    for keys in redis.keys('binance_*'):
-        binance_dict[keys[8:]] = redis.get(keys)
-    return binance_dict
-
-print(get_binance_dict())
+# идем по списку монет и достаем только нужные нам
+for coin in data['symbols']:
+    # проверяем открыта ли торговля и можно ли торговать на споте
+    if coin['status'] == 'TRADING' and coin['isSpotTradingAllowed']:
+        print(coin)
+        print(coin['baseAsset'], coin['quoteAsset'])
--- ws_to_redis/__main__.py
+++
@@ -1,6 +0,0 @@
-from ws_to_redis.websocket.ticker import ws
-
-if __name__ == '__main__':
-    while True:
-        ws.run_forever()
-
